f005e87c910b9421845263ab7d364428324f57fd
Julian Kohn <julian.kohn@hotmail.de>
2025-08-30 13:56:57 +0200

    add skill component content

--- src/app/main-page/skills/skills.ts
+++ src/app/main-page/skills/skills.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
 
+interface Skill {
+  name: string;
+  img: string;
+}
+
 @Component({
   selector: 'app-skills',
   imports: [],
   templateUrl: './skills.html',
   styleUrl: './skills.scss'
 })
-export class Skills {
 
+export class Skills {
+  skills: Skill[] = [
+    { name: 'HTML', img: '/img/skills/skill_html.png' },
+    { name: 'CSS', img: '/img/skills/skill_css.png' },
+    { name: 'JavScript', img: '/img/skills/skill_javscript.png' },
+    { name: 'Git', img: '/img/skills/skill_git.png' },
+    { name: 'Angular', img: '/img/skills/skill_angular.png' },
+    { name: 'TypeScript', img: '/img/skills/skill_typescript.png' },
+    { name: 'API', img: '/img/skills/skill_api.png' },
+    { name: 'Firebase', img: '/img/skills/skill_firebase.png' },
+    { name: 'Scrum', img: '/img/skills/skill_scrum.png' },
+  ];
 }
