5b5cc27eb3390a2bf402931a5b76ca15ff1e37dd
Julian Kohn <julian.kohn@hotmail.de>
2025-09-02 01:14:51 +0200

    add portfolio component content / ather component fixes

--- src/app/main-page/portfolio/portfolio.ts
+++ src/app/main-page/portfolio/portfolio.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+interface Projects {
+  name: string;
+  languages: string;
+  description: string;
+  img: string,
+  linkgit: string;
+  linktest: string
+}
+
 @Component({
   selector: 'app-portfolio',
   imports: [],
@@ -7,5 +16,34 @@ import { Component } from '@angular/core';
   styleUrl: './portfolio.scss'
 })
 export class Portfolio {
+  allProjects: Projects[] = [
+    {
+      name: 'Join',
+      languages: 'Angular | TypeScript | HTML | CSS | Firebase',
+      description: 'Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories. ',
+      img: './img/projects/projects_join.png',
+      linkgit: '',
+      linktest: ''
+    },
+    {
+      name: 'El Dragonnoid',
+      languages: 'JavaScript | HTML | CSS',
+      description: 'A simple Jump-and-Run game based on an object-oriented approach. Help sharkie to find coins and poison bottles to fight against the killer whale.',
+      img: './img/projects/projects_el_dragonnoid.png',
+      linkgit: '',
+      linktest: ''
+    },
+    {
+      name: 'Pokédex',
+      languages: 'JavaScript | HTML | CSS | Api',
+      description: 'Based on the PokéAPI a simple library that provides and catalogues pokemon information.',
+      img: './img/projects/projects_pokedex.png',
+      linkgit: '',
+      linktest: ''
+    },
+  ];
 
+  openLink(url: string) {
+    window.open(url, "_blank");
+  }
 }
