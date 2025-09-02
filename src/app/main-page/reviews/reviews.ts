80677a77f854dfcdae6d4d4901db6551bbbb207a
Julian Kohn <julian.kohn@hotmail.de>
2025-09-02 18:21:40 +0200

    add skills component content

--- src/app/main-page/reviews/reviews.ts
+++ src/app/main-page/reviews/reviews.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+interface Review {
+  name: string;
+  review: string;
+  img: string,
+}
+
 @Component({
   selector: 'app-reviews',
   imports: [],
@@ -7,5 +13,21 @@ import { Component } from '@angular/core';
   styleUrl: './reviews.scss'
 })
 export class Reviews {
-
+  allReviews: Review[] = [
+    {
+      name: "V. Schuster - Team Partner",
+      review: "Michael really kept the team together with his great organization and clear communication. We wouldn't have got this far without his commitment.",
+      img: "./img/pictures/picture_placeholder_02.png"
+    },
+    {
+      name: "V. Schuster - Team Partner",
+      review: "Michael really kept the team together with his great organization and clear communication. We wouldn't have got this far without his commitment.",
+      img: "./img/pictures/picture_placeholder_03.png"
+    },
+    {
+      name: "V. Schuster - Team Partner",
+      review: "Michael really kept the team together with his great organization and clear communication. We wouldn't have got this far without his commitment.",
+      img: "./img/pictures/picture_placeholder_04.png"
+    },
+  ];
 }
