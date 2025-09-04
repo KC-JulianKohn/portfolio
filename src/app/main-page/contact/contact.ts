ba06a288a6a9ac8585813d1f140201d2fdda2ef6
Julian Kohn <julian.kohn@hotmail.de>
2025-09-04 18:41:51 +0200

    add contact component content

--- src/app/main-page/contact/contact.ts
+++ src/app/main-page/contact/contact.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './contact.html',
   styleUrl: './contact.scss'
 })
 export class Contact {
+  username: string = '';
+  email: string = '';
+  message: string = '';
+  accepted: boolean = false;
+  submitted: boolean = false;
 
+  sendMessage() {
+    this.submitted = true;
+    if (!this.accepted) {
+
+    } else {
+      this.submitted = false;
+    }
+  }
 }
