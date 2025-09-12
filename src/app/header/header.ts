ce01384017d47ac74d72be3f49c6cecfe486f951
Julian Kohn <julian.kohn@hotmail.de>
2025-09-12 02:50:04 +0200

    add "bürger menue"  in header component

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, HostListener, inject } from '@angular/core';
 import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 
 @Component({
@@ -9,4 +9,44 @@ import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 })
 export class Header {
   translate = inject(TranslateService);
+
+  isMobile = false;
+  menuOpen = false;
+
+  constructor() {
+    this.checkViewport();
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.checkViewport();
+  }
+
+  private checkViewport() {
+    this.isMobile = window.innerWidth <= 900;
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+
+    if (this.menuOpen) {
+      const scrollY = window.scrollY;
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${scrollY}px`;
+      document.body.style.width = '100%';
+    } else {
+      const scrollY = Math.abs(parseInt(document.body.style.top || '0', 10));
+      document.body.style.position = '';
+      document.body.style.top = '';
+      window.scrollTo(0, scrollY);
+    }
+  }
+
+  closeMenu() {
+    this.menuOpen = false;
+    const scrollY = Math.abs(parseInt(document.body.style.top || '0', 10));
+    document.body.style.position = '';
+    document.body.style.top = '';
+    window.scrollTo(0, scrollY);
+  }
 }
